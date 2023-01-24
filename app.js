cd6dde0a674049c3cb7523ebcd777a6bedad9497
samano2j <jemsamano@yahoo.com>
2023-01-24 14:55:47 -0600

    Finished Exercise

---
+++ app.js
@@ -0,0 +1,39 @@
+//Adding a note
+
+document.getElementById("add-btn").addEventListener("click", (e) => {
+    var list = document.getElementById("list");
+    var note = document.getElementById("add-input").value;
+    console.log(note);
+
+    const newItem = document.createElement("li");
+
+    const noteItem = document.createElement("p");
+    noteItem.innerText = note;
+
+    const iconItems = document.createElement("p");
+
+    const icon1 = document.createElement("i");
+    icon1.classList.add("fa");
+    icon1.classList.add("fa-pencil-square-o");
+
+    const icon2 = document.createElement("i");
+    icon2.classList.add("fa");
+    icon2.classList.add("fa-times");
+
+    const editNote = document.createElement("input");
+    editNote.setAttribute('type','text');
+    editNote.classList.add("edit-note");
+
+    iconItems.appendChild(icon1);
+    iconItems.appendChild(icon2);
+
+    newItem.appendChild(noteItem);
+    newItem.appendChild(iconItems);
+    newItem.appendChild(editNote);
+    list.appendChild(newItem);
+    document.getElementById("add-input").value = "";
+
+    console.log(list);
+    event.preventDefault();
+});
+  
